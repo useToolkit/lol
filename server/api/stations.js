470f44c2c0b5b24ce50016971a22f8eaa9a5307f
useToolkit <147295947+useToolkit@users.noreply.github.com>
2026-09-21 17:10:23 +0900

    FuelPick server: update stations helper import

--- server/api/stations.js
+++ server/api/stations.js
@@ -1,4 +1,4 @@
-import { checkClient, normalizeAround, opinet, send, validateFuel, wgsToKatec } from "./_lib.js";
+import { checkClient, normalizeAround, opinet, send, validateFuel, wgsToKatec } from "../lib/opinet.js";
 
 export default async function handler(req,res){
   if(req.method!=="GET") return send(res,405,{error:"METHOD_NOT_ALLOWED"});
